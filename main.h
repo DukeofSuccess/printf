294492eadb1a7464d2f41163b519e9727007d2af
GabrielaDaSilva <gabrieladasilva70@gmail.com>
2023-03-27 10:48:20 -0700

    main.c

--- main.h
+++ main.h
@@ -67,11 +67,11 @@ int print_hexa(va_list types, char map_to[],
 int print_non_printable(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
-/* Funcion to print memory address */
+/* Function to print memory address */
 int print_pointer(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
 
-/* Funciotns to handle other specifiers */
+/* Functions to handle other specifiers */
 int get_flags(const char *format, int *i);
 int get_width(const char *format, int *i, va_list list);
 int get_precision(const char *format, int *i, va_list list);
